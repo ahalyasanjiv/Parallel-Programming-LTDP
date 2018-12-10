3a3f1b1ab5b350fe2240067d2053b8e3908a7680
Ahalya Sanjiv <ahalyasanjiv@gmail.com>
2018-12-10 14:07:52 -0500

    Edited user prompt message

--- smith-waterman/smith_waterman_ltdp.c
+++ smith-waterman/smith_waterman_ltdp.c
@@ -397,7 +397,7 @@ int main() {
   srand(time(NULL));
   int ref_len,query_len;
   printf("===========================================================\n"
-         "SMITH WATERMAN PARALLEL ALGORITHM\n");
+         "SMITH WATERMAN LTDP PARALLEL ALGORITHM\n");
   printf("Computing the local alignment for two random DNA sequences.\n");
   printf("===========================================================\n");
   printf("Enter the string size for the reference string: ");
