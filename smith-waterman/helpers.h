78b3ad47e91c7459b2b97b766df8956320d94d28
Ahalya Sanjiv <ahalyasanjiv@gmail.com>
2018-12-09 23:07:44 -0500

    Added more helper functions from LTDP implementation

--- smith-waterman/helpers.h
+++ smith-waterman/helpers.h
@@ -9,6 +9,10 @@ void print_reverse(char* x, int x_len);
 void print_matrix(int m, int n, int A[m][n]);
 char* generateSequence(int n);
 int get_num_of_elements_in_stage(int i, int x_len, int y_len);
+int get_num_of_stages(int m, int n);
+int get_start_row(int j,int m);
+int get_start_col(int j,int m);
+void copy_new_soln(int m, int n, int start_i, int start_j, int new_score[m][n], int new_pred[m][n], int score[m][n], int pred[m][n]);
 
 /* Finds the max of two integers */
 int max(int a, int b) {
@@ -75,5 +79,47 @@ int get_num_of_elements_in_stage(int i, int x_len, int y_len) {
     return min_dim;
   } else {
     return 2*min_dim-i+(max_dim-min_dim)-1;
+  }
+}
+
+int get_num_of_stages(int m, int n) {
+  return (m-2) + (n-2) + 1;
+}
+
+int get_start_row(
+  int j,
+  int m
+) {
+  if (j < m - 1){
+    return j+1;
+  }
+  return m - 1;
+}
+
+int get_start_col(
+  int j,
+  int m
+) {
+  if (j < m - 1){
+    return 1;
+  }
+  return j - m + 3;
+}
+
+void copy_new_soln(
+  int m,
+  int n,
+  int start_i,
+  int start_j,
+  int new_score[m][n],
+  int new_pred[m][n],
+  int score[m][n],
+  int pred[m][n]
+) {
+  int i = start_i;
+  for (int j = start_j; j < n && i > 0; j++) {
+    score[i][j] = new_score[i][j];
+    pred[i][j] = new_pred[i][j];
+    i--;
   }
 }
