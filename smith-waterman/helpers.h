828456b6f7bdb889d05651034bb93d80e17d9aa6
Ahalya Sanjiv <ahalyasanjiv@gmail.com>
2018-12-09 22:51:56 -0500

    Added more aux functions

--- smith-waterman/helpers.h
+++ smith-waterman/helpers.h
@@ -4,13 +4,22 @@
 #include <time.h>
 
 int max(int a, int b);
+int min(int a, int b);
 void print_reverse(char* x, int x_len);
 void print_matrix(int m, int n, int A[m][n]);
 char* generateSequence(int n);
+int get_num_of_elements_in_stage(int i, int x_len, int y_len);
 
 /* Finds the max of two integers */
 int max(int a, int b) {
   if (a > b) {
+    return a;
+  }
+  return b;
+}
+
+int min(int a, int b) {
+  if (a < b) {
     return a;
   }
   return b;
@@ -48,4 +57,23 @@ char* generateSequence(
   }
   seq[n] = '\0';
   return seq;
+}
+
+// Get number of elements in stage based on stage number and lengths of strings
+int get_num_of_elements_in_stage(int i, int x_len, int y_len) {
+  int max_dim, min_dim;
+  if (x_len > y_len) {
+    max_dim = x_len;
+    min_dim = y_len;
+  } else {
+    max_dim = y_len;
+    min_dim = x_len;
+  }
+  if (min_dim > i) {
+    return i + 1;
+  } else if (max_dim > i) {
+    return min_dim;
+  } else {
+    return 2*min_dim-i+(max_dim-min_dim)-1;
+  }
 }
