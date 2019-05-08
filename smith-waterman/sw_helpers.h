e3af74c5c2bbc76de108135758c401b750a4dcde
Ahalya Sanjiv <ahalyasanjiv@gmail.com>
2019-05-07 22:48:32 -0400

    Hybrid OpenMP + MPI dp programs

---
+++ smith-waterman/sw_helpers.h
@@ -0,0 +1,288 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <time.h>
+#include <mpi.h>
+#include <omp.h>
+
+int max(int a, int b);
+int min(int a, int b);
+void print_reverse(char* x, int x_len);
+void print_matrix(int m, int n, int A[m][n]);
+void calculate_element(int up, int diagonal, int left, int * score, int * pred);
+char* generate_ref_sequence(int ref_len, int ref_seq_len );
+char* generate_query_sequence(int n);
+int get_num_of_elements_in_stage(int i, int x_len, int y_len);
+int get_num_of_stages(int m, int n);
+int get_start_row(int j,int m);
+int get_start_col(int j,int m);
+void copy_new_soln(int m, int n, int start_i, int start_j, int new_score[m][n], int new_pred[m][n], int score[m][n], int pred[m][n]);
+bool is_parallel( int m, int n, int start_i, int start_j, int new_score[m][n], int score[m][n]);
+
+/*
+ * Function: max
+ * --------------------
+ *  Returns max of two integers
+ *
+ *  a: first integer
+ *  b: second integer
+ *
+ *  returns: the larger of a and b
+ */
+int max(int a, int b) {
+  if (a > b) {
+    return a;
+  }
+  return b;
+}
+
+/*
+ * Function: min
+ * --------------------
+ *  Returns min of two integers
+ *
+ *  a: first integer
+ *  b: second integer
+ *
+ *  returns: the smaller of a and b
+ */
+int min(int a, int b) {
+  if (a < b) {
+    return a;
+  }
+  return b;
+}
+
+/*
+ * Function: print_reverse
+ * --------------------
+ *  Prints reverse of string
+ *
+ *  x: string to print in reverse
+ *  x_len: length of x
+ */
+void print_reverse(char* x, int x_len) {
+  char* new_str = malloc(x_len);
+  int i;
+  for (i=0; i<x_len; i++) {
+    new_str[i] = x[x_len-i-1];
+  }
+  new_str[i] = '\0';
+  printf("%s\n", new_str);
+  free(new_str);
+}
+
+/*
+ * Function: print_matrix
+ * --------------------
+ *  Prints matrix
+ *
+ *  m: num of rows in A
+ *  n: num of cols in A
+ *  A: matrix to print
+ */
+void print_matrix(int m, int n, int A[m][n]) {
+  for (int i=0; i<m; i++) {
+    for (int j=0; j<n; j++) {
+      printf("%d ", A[i][j]);
+    }
+    printf("\n");
+  }
+}
+
+/*
+ * Function: calculate_element
+ * --------------------
+ * Calculates element in scoring matrix and predecessor matrix
+ *
+ */
+void calculate_element(int up, int diagonal, int left, int * score, int * pred) {
+  // Set score & predecessor for current subproblem
+  if ((max(0,max(diagonal,max(up,left)))) == 0) {
+    *score = 0;
+    *pred = 0;
+  } else if (diagonal >= up && diagonal >= left) {
+    *score = diagonal;
+    *pred = 1;
+  } else if (left >= diagonal && left >= up) {
+    *score = left;
+    *pred = 2;
+  } else {
+    *score = up;
+    *pred = 3;
+  }
+}
+
+/*
+ * Function: generate_query_sequence
+ * --------------------
+ *  Randomly generates a DNA sequence of length n
+ *
+ *  n: length of
+ *
+ *  returns: randomly generated DNA sequence of length n
+ */
+char* generate_query_sequence(
+  int n
+) {
+  char* seq = malloc(sizeof(char) * (n + 1));
+  char seqChars[] = {'A','C','G','T'};
+  for (int i=0; i<n; i++) {
+    seq[i] = seqChars[rand()%4];
+  }
+  seq[n] = '\0';
+  return seq;
+}
+
+char* generate_ref_sequence(
+  int ref_len,
+  int ref_seq_len
+) {
+  char* ref = malloc(sizeof(char) * ((ref_len * ref_seq_len)+1));
+  char seqChars[] = {'A','C','G','T'};
+  for (int i; i<ref_len*ref_seq_len; i++) {
+      ref[i] = seqChars[rand()%4];
+  }
+  ref[ref_len*ref_seq_len] = '\0';
+  return ref;
+}
+
+//
+
+/*
+ * Function: get_num_of_elements_in_stage
+ * --------------------
+ *  Get number of elements in stage based on stage number and lengths of strings
+ *
+ *  i: the stage index
+ *  x_len: length of first string x
+ *  y_len: length of second string y
+ *
+ *  returns: number of elements in stage i
+ */
+int get_num_of_elements_in_stage(int i, int x_len, int y_len) {
+  int max_dim, min_dim;
+  if (x_len > y_len) {
+    max_dim = x_len;
+    min_dim = y_len;
+  } else {
+    max_dim = y_len;
+    min_dim = x_len;
+  }
+  if (min_dim > i) {
+    return i + 1;
+  } else if (max_dim > i) {
+    return min_dim;
+  } else {
+    return 2*min_dim-i+(max_dim-min_dim)-1;
+  }
+}
+
+/*
+ * Function: get_num_of_stages
+ * --------------------
+ *  Get number of stages in a matrix
+ *
+ *  m: num of rows in matrix
+ *  n: num of cols in matrix
+ *
+ *  returns: number of stages in a m by n matrix
+ */
+int get_num_of_stages(int m, int n) {
+  return (m-2) + (n-2) + 1;
+}
+
+/*
+ * Function: get_start_row
+ * --------------------
+ *  Get start row index for a stage
+ *
+ *  j: stage index
+ *  m: num of rows in matrix
+ *
+ *  returns: start row index for stage j
+ */
+int get_start_row(
+  int j,
+  int m
+) {
+  if (j < m - 1){
+    return j+1;
+  }
+  return m - 1;
+}
+
+/*
+ * Function: get_start_col
+ * --------------------
+ *  Get start col index for a stage
+ *
+ *  j: stage index
+ *  m: num of rows in matrix
+ *
+ *  returns: start col index for stage j
+ */
+int get_start_col(
+  int j,
+  int m
+) {
+  if (j < m - 1){
+    return 1;
+  }
+  return j - m + 3;
+}
+
+/*
+ * Function: copy_new_soln
+ * --------------------
+ *  Copies a new solution vectors to an old solution vector in score and pred
+ *
+ *  m: num of rows in matrix
+ *  n: num of cols in matrix
+ *  start_i: starting row index of solution vector
+ *  start_j: starting col index of solution vector
+ *  new_score: score matrix with new solution vector
+ *  new_pred: predecessor matrix with new solution vector
+ *  score: score matrix with old solution vector
+ *  pred: predecessor matrix with old solution vector
+ */
+void copy_new_soln(
+  int m,
+  int n,
+  int start_i,
+  int start_j,
+  int new_score[m][n],
+  int new_pred[m][n],
+  int score[m][n],
+  int pred[m][n]
+) {
+  int i = start_i;
+  for (int j = start_j; j < n && i > 0; j++) {
+    score[i][j] = new_score[i][j];
+    pred[i][j] = new_pred[i][j];
+    i--;
+  }
+}
+
+bool is_parallel(
+  int m,
+  int n,
+  int start_i,
+  int start_j,
+  int new_score[m][n],
+  int score[m][n]
+) {
+  bool parallel = true;
+
+  for (int i = 0; start_j+i < n-1 && start_i - i > 1; i++) {
+    if ((new_score[start_i-i][start_j+i] - score[start_i-i][start_j+i]) != (new_score[start_i-i][start_j+i] - score[start_i-i-1][start_j+i+1])) {
+      parallel = false;
+    }
+    else {
+      parallel = true;
+    }
+  }
+
+  return parallel;
+}
